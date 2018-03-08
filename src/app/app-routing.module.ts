4be178755dabf9f44349f14287f5d6057acbb5d6
chrystian cavalcante <chrystiancavalcante@gmail.com>
2018-03-08 07:47:23 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CadastroComponent } from './cadastro/cadastro.component';
+
 
 const routes: Routes = [
-  { path: 'cadastro', component: CadastroComponent },
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './login/login.component';
-import { SystemComponent } from './pages/system/system.component';
 import { VendaComponent } from './system/venda/venda.component';
 import { EstoqueComponent } from './system/estoque/estoque.component';
 
@@ -16,11 +14,9 @@ import { EstoqueComponent } from './system/estoque/estoque.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroComponent,
     AboutComponent,
     HomeComponent,
     LoginComponent,
-    SystemComponent,
     VendaComponent,
     EstoqueComponent
   ],

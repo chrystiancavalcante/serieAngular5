9ba246c865603e97ce557de02013cfaf6b2ed13a
chrystian cavalcante <chrystiancavalcante@gmail.com>
2018-03-08 07:18:00 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SystemComponent } from './pages/system/system.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CadastroComponent
+    CadastroComponent,
+    AboutComponent,
+    HomeComponent,
+    LoginComponent,
+    SystemComponent
   ],
   imports: [
     BrowserModule,
